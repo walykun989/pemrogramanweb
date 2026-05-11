5528d9562b5fb2a165628279094a679d11d0ecdb
zaharoZ <walykun989@gmail.com>
2026-05-11 13:45:40 +0700

    UTS praktikum

---
+++ UTS-Frontend/script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-lanjut').addEventListener('click', function() {
+
+    document.getElementById('intro-screen').style.display = 'none';
+    
+    document.getElementById('main-content').style.display = 'block';
+});
